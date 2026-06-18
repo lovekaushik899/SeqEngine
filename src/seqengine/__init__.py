14e939c4304b4b897dbc7ca45f4d8b2dc118392f
Love Kaushik <130579185+lovekaushik899@users.noreply.github.com>
2026-06-19 00:39:43 +0530

    Create __init__.py

---
+++ src/seqengine/__init__.py
@@ -0,0 +1,10 @@
+"""
+SeqEngine: Automated Feature Engineering & Selection for Protein and DNA Sequences
+"""
+
+__version__ = "1.0.0"
+__author__ = "Love Kaushik"
+
+from .engine import main
+
+__all__ = ["main"]
